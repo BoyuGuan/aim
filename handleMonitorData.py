19fb1835d79b221be945fb858aeb154e2571d963
Jack Guan <cnboyuguan@gmail.com>
2022-04-15 12:00:56 +0800

    add log info

--- handleMonitorData.py
+++ handleMonitorData.py
@@ -14,10 +14,11 @@ def findMax(filen):
 if __name__ == '__main__':
     ans = []
     count = 0
-    with  open('./monitorLog/resnet50.log', mode='r') as f:
+    with  open('./monitorLog/channel_resnet50.txt', mode='r') as f:
         lines = f.readlines()
-        for trueFilen in [lines[:995], lines[995:1927], lines[1927:2744], lines[2744:3617]]:
+        sections = [ 1023, 1897, 2719, 3343, 4336, 5160, 5956]
+        for trueFilen in [lines[:sections[0]], lines[sections[0]:sections[1]], lines[sections[1]:sections[2]], lines[sections[2]:sections[3]]]:
             ans.append(findMax(trueFilen))
-        for falseFilen in [lines[3617:4652], lines[4652:5476], lines[5476:6348], lines[6348: 8200]]:
+        for falseFilen in [lines[sections[3]:sections[4]], lines[sections[4]:sections[5]], lines[sections[5]:sections[6]], lines[sections[6]:]]:
             ans.append(findMax(falseFilen))
     print(ans)
