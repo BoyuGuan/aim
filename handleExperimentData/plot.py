a1617c92bb818d0f45de48811decaff5536f5f65
Jack Guan <cnboyuguan@gmail.com>
2022-05-02 21:42:34 +0800

    make plot more beautiful

--- handleExperimentData/plot.py
+++ handleExperimentData/plot.py
@@ -1,17 +1,36 @@
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
 PLOTDIR = './handleExperimentData/plotImages'
 os.makedirs(PLOTDIR, exist_ok=True)
 
 def channel_resnet18():
-    MACRatio = [0.302908, 0.545076, 0.649888, 0.665484, 0.197297, 0.399185, 0.473236, 0.621336]
-    memoryRatio = [0.198605, 0.241760, 0.296652, 0.389077, 0.160678, 0.210540,  0.226964, 0.262622]
-    accBeforeFinetune = [43.869427, 74.343153, 84.056529, 83.588774, 29.319268, 57.314889, 48.029459, 82.931927]
-    accAfterFinetune = [86.17, 88.37, 88.66, 88.48, 84.45, 87.42, 87.82, 88.63]
-    costTime = [ 863.702305316925, 817.5897779464722,  839.0135326385498, 823.8756124973297, 858.6545453071594, 769.8132638931274, 862.3200585842133, 808.2758123874664]
+    MACRatio = np.array([0.302908, 0.545076, 0.649888, 0.665484, 0.197297, 0.399185, 0.473236, 0.621336])
+    memoryRatio = np.array([0.198605, 0.241760, 0.296652, 0.389077, 0.160678, 0.210540,  0.226964, 0.262622])
+    accBeforeFinetune = np.array([43.869427, 74.343153, 84.056529, 83.588774, 29.319268, 57.314889, 48.029459, 82.931927])
+    accAfterFinetune = np.array([86.17, 88.37, 88.66, 88.48, 84.45, 87.42, 87.82, 88.63])
+    costTime = np.array([ 863.702305316925, 817.5897779464722,  839.0135326385498, 823.8756124973297, 858.6545453071594, 769.8132638931274, 862.3200585842133, 808.2758123874664])
 
-    plt.plot( MACRatio, costTime )
+    fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(10, 10))
+    MACRatioSortedIndex, MACRatioSorted = zip(* sorted([(i, x) for (i ,x) in enumerate(MACRatio) ], key=lambda x : x[1]) ) 
+    axs[0, 0].plot( MACRatioSorted, costTime[np.array(MACRatioSortedIndex)] , '^k:')
+    axs[0, 0].set_xlabel('compression ratio (MAC)')
+    axs[0, 0].set_ylabel('algorithm time cost (s)')
+    memoryRatioSortedIndex, memoryRatioSorted = zip(* sorted([(i, x) for (i ,x) in enumerate(memoryRatio) ] , key=lambda x : x[1]) ) 
+    axs[0, 1].plot( memoryRatioSorted, costTime[np.array(memoryRatioSortedIndex)], '^k:')
+    axs[0, 1].set_xlabel('compression ratio (memory)')
+    axs[0, 1].set_ylabel('algorithm time cost (s)')
+    axs[1, 0].plot( MACRatioSorted, accBeforeFinetune[np.array(MACRatioSortedIndex)] , '8-.c',MACRatioSorted, accAfterFinetune[np.array(MACRatioSortedIndex)] , 's--y' )
+    axs[1, 0].set_xlabel('compression ratio (MAC)')
+    axs[1, 0].set_ylabel('accuracy on validation dataset(%)')
+    axs[1, 1].plot( memoryRatioSorted, accBeforeFinetune[np.array(memoryRatioSortedIndex)] , '8-.c',memoryRatioSorted, accAfterFinetune[np.array(memoryRatioSortedIndex)] , 's--y' )
+    axs[1, 1].set_xlabel('compression ratio (MAC)')
+    axs[1, 1].set_ylabel('accuracy on validation dataset(%)')
+
+
+    fig.set(tight_layout = True)
+    # fig.set(dpi = 600)
     plt.savefig(PLOTDIR + '/channel_resnet18.png')
 
 
