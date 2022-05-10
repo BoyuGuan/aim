f4a2a7ddc19ed8704574bbf0523e9ee71175748a
Jack Guan <cnboyuguan@gmail.com>
2022-05-10 17:10:13 +0800

    更新了experiments里Weight SVD 18 的数据，plot里相关数据更新了一半

--- handleExperimentData/plot.py
+++ handleExperimentData/plot.py
@@ -68,6 +68,40 @@ def channel_resnet50():
     fig.set(tight_layout = True)
     plt.savefig(PLOTDIR + '/channel_resnet50.png')
 
+def weightSVD_resnet18():
+    MACRatio = np.array([0.471318, 0.156471, 0.282467, 0.461664])
+    memoryRatio = np.array([0.174902, 0.098078, 0.112759, 0.165290])
+    accBeforeFinetune = np.array([78.025478, 21.725717, 36.275876, 73.377787])
+    accAfterFinetune = np.array([87.99, 84.92, 87.0, 88.34])
+    costTime = np.array([ 3309.1602478027344, 3944.231453895569,  3270.3850288391113, 3158.2146010398865, 4006.2553544044495, 3466.395381450653, 3259.653082847595, 2472.8123886585236])
+
+    fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(10, 10))
+    MACRatioSortedIndex, MACRatioSorted = zip(* sorted([(i, x) for (i ,x) in enumerate(MACRatio) ], key=lambda x : x[1]) ) 
+    axs[0, 0].plot( MACRatioSorted, costTime[np.array(MACRatioSortedIndex)] , '^k:')
+    axs[0, 0].set_xlabel('compression ratio (MAC)')
+    axs[0, 0].set_ylabel('algorithm time cost (s)')
+    memoryRatioSortedIndex, memoryRatioSorted = zip(* sorted([(i, x) for (i ,x) in enumerate(memoryRatio) ] , key=lambda x : x[1]) ) 
+    axs[0, 1].plot( memoryRatioSorted, costTime[np.array(memoryRatioSortedIndex)], '^k:')
+    axs[0, 1].set_xlabel('compression ratio (memory)')
+    axs[0, 1].set_ylabel('algorithm time cost (s)')
+    axs[1, 0].plot( MACRatioSorted, [90.2] * len(MACRatioSorted) , ',-r' , label='baselien accuracy')
+    axs[1, 0].plot( MACRatioSorted, accAfterFinetune[np.array(MACRatioSortedIndex)] , 's--y' , label='accuracy before finetune')
+    axs[1, 0].plot( MACRatioSorted, accBeforeFinetune[np.array(MACRatioSortedIndex)] , '8-.c', label='accuracy before finetune')
+    axs[1, 0].set_xlabel('compression ratio (MAC)')
+    axs[1, 0].set_ylabel('accuracy on validation dataset(%)')
+    axs[1, 0].legend()
+    axs[1, 1].plot( memoryRatioSorted, [90.2] * len(MACRatioSorted) , ',-r' , label='baselien accuracy')
+    axs[1, 1].plot(memoryRatioSorted, accAfterFinetune[np.array(memoryRatioSortedIndex)] , 's--y', label='accuracy after finetune')
+    axs[1, 1].plot( memoryRatioSorted, accBeforeFinetune[np.array(memoryRatioSortedIndex)] , '8-.c', label='accuracy before finetune')
+    axs[1, 1].set_xlabel('compression ratio (memory)')
+    axs[1, 1].set_ylabel('accuracy on validation dataset(%)')
+    axs[1, 1].legend()
+    fig.set(tight_layout = True)
+    plt.savefig(PLOTDIR + '/channel_resnet50.png')
+
+
+
+
 
 if __name__ == '__main__':
     # channel_resnet18()
