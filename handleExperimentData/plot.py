7dd28206189b23286ed230ecd33b89b003796731
Jack Guan <cnboyuguan@gmail.com>
2022-05-20 15:48:41 +0800

    完成显存内存绘图

--- handleExperimentData/plot.py
+++ handleExperimentData/plot.py
@@ -194,8 +194,8 @@ def spatialSVD_resnet50():
     plt.savefig(PLOTDIR + '/spatialSVD_resnet50.png')
 
 def adaround():
-    accResNet18 = [10.25, 72.52, 76.58]
-    accResNet50 = [77.09, 84.98, 85.33]
+    accResNet18 = [10.52, 72.52, 67.58]
+    accResNet50 = [76.77, 84.98, 85.33]
     # x = [ 1, 2, 3 ]
     x = [ '4bit', '8bit', '16bit' ]
 
@@ -264,37 +264,45 @@ def cle_bc_resnet50():
     plt.legend(loc='lower right', shadow=True )
     plt.savefig(PLOTDIR + '/cle_bc_resnet50.png')
 
-def compare_time_resnet18():
+def compare_time():
     # plt.figure(figsize=(20, 6), dpi=180)
-    channel_time_resnet18 = np.array([863.70, 817.59, 839.01, 823.88, 769.81, 862.32, 808.28]).mean()
-    weight_svd_resnet18 = np.array([246.6894, 248.419, 245.428, 245.516]).mean()
-    spatial_svd_resnet18 = np.array([144.548, 146.545, 146.413, 146.24, 140.435]).mean()
-    adaround_resnet18 = np.array([423.72, 422.85, 416.426]).mean()
-    CLE_time_ResNet18 = np.array([10.25, 10.25, 10.25]).mean()
-    bc_time_ResNet18 = np.array([19.02, 18.92, 19.29]).mean()
+    channel_time_resnet18 = np.array([863.70, 817.59, 839.01, 823.88, 769.81, 862.32, 808.28])
+    weight_svd_resnet18 = np.array([246.6894, 248.419, 245.428, 245.516])
+    spatial_svd_resnet18 = np.array([144.548, 146.545, 146.413, 146.24, 140.435])
+    adaround_resnet18 = np.array([425.87, 422.48, 429.46])
+    CLE_time_ResNet18 = np.array([10.25, 10.25, 10.25])
+    bc_time_ResNet18 = np.array([19.02, 18.92, 19.29])
 
-    channel_time_resnet50 = np.array([3309.16, 3944.23, 3270.385, 3158.2, 4006.255, 3466.395, 3259.653, 2472.81]).mean()
-    weight_svd_resnet50 = np.array([754.11, 747.958, 744.14]).mean()
-    spatial_svd_resnet50 = np.array([144.548, 146.545, 146.413, 146.24, 140.435]).mean()
-    adaround_resnet50 = np.array([1121.21, 1125.66, 1145.067]).mean()
-    CLE_time_ResNet50 = np.array([26.34, 26.253, 26.35]).mean()
-    bc_time_ResNet50 = np.array([53.9424, 53.7026, 53.73]).mean()
+    channel_time_resnet50 = np.array([3309.16, 3944.23, 3270.385, 3158.2, 4006.255, 3466.395, 3259.653, 2472.81])
+    weight_svd_resnet50 = np.array([754.11, 747.958, 744.14])
+    spatial_svd_resnet50 = np.array([144.548, 146.545, 146.413, 146.24, 140.435])
+    adaround_resnet50 = np.array([1139.88, 1114.346, 1122.76])
+    CLE_time_ResNet50 = np.array([26.34, 26.253, 26.35])
+    bc_time_ResNet50 = np.array([53.9424, 53.7026, 53.73])
 
     colName = ['C P', 'W SVD', 'S SVD', 'AdaRound', 'CLE', 'BC','CLE + BC' ]
     barPositions = (np.arange(7) + 0.75) * (-1)
     tickPositions = barPositions
     fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(10, 12))
 
-    axs[0].barh(barPositions, [ channel_time_resnet18, weight_svd_resnet18, spatial_svd_resnet18, adaround_resnet18,\
-         CLE_time_ResNet18, bc_time_ResNet18, CLE_time_ResNet18 + bc_time_ResNet18], 0.5, color = '#905a3d')
+    resnet18_xerr = [channel_time_resnet18.max() -  channel_time_resnet18.min(),\
+             weight_svd_resnet18.max()- weight_svd_resnet18.min(), spatial_svd_resnet18.max() - spatial_svd_resnet18.min(), adaround_resnet18.max() - adaround_resnet18.min(), \
+                 CLE_time_ResNet18.max() - CLE_time_ResNet18.min(), bc_time_ResNet18.max() - bc_time_ResNet18.min(), (CLE_time_ResNet18 + bc_time_ResNet18).max() - (CLE_time_ResNet18 + bc_time_ResNet18).min()] 
+    resnet18_xerr = [0.5*x for x in resnet18_xerr]
+    axs[0].barh(barPositions, [ channel_time_resnet18.mean(), weight_svd_resnet18.mean(), spatial_svd_resnet18.mean(), adaround_resnet18.mean(),\
+         CLE_time_ResNet18.mean(), bc_time_ResNet18.mean(), (CLE_time_ResNet18 + bc_time_ResNet18).mean()], 0.5, xerr = resnet18_xerr, align='center', color = '#905a3d')
     axs[0].set_yticks(tickPositions)
     axs[0].set_yticklabels(colName)
     axs[0].set_title('Time Cost on ResNet18')
     axs[0].set_xlabel('Time (s)')
     axs[0].set_ylabel('Algorithm Name')
 
-    axs[1].barh(barPositions, [ channel_time_resnet50, weight_svd_resnet50, spatial_svd_resnet50, adaround_resnet50,\
-         CLE_time_ResNet50, bc_time_ResNet50, CLE_time_ResNet50 + bc_time_ResNet50], 0.5, color = '#905a3d')
+    resnet50_xerr = [channel_time_resnet50.max() -  channel_time_resnet50.min(),\
+             weight_svd_resnet50.max()- weight_svd_resnet50.min(), spatial_svd_resnet50.max() - spatial_svd_resnet50.min(), adaround_resnet50.max() - adaround_resnet50.min(), \
+                 CLE_time_ResNet50.max() - CLE_time_ResNet50.min(), bc_time_ResNet50.max() - bc_time_ResNet50.min(), (CLE_time_ResNet50 + bc_time_ResNet50).max() - (CLE_time_ResNet50 + bc_time_ResNet50).min() ]
+    resnet50_xerr = [ 0.5 * x for x in resnet50_xerr]
+    axs[1].barh(barPositions, [ channel_time_resnet50.mean(), weight_svd_resnet50.mean(), spatial_svd_resnet50.mean(), adaround_resnet50.mean(),\
+         CLE_time_ResNet50.mean(), bc_time_ResNet50.mean(), CLE_time_ResNet50.mean() + bc_time_ResNet50.mean()], 0.5, xerr = resnet50_xerr, color = '#905a3d')
     axs[1].set_yticks(tickPositions)
     axs[1].set_yticklabels(colName)
     axs[1].set_title('Time Cost on ResNet50')
@@ -302,6 +310,66 @@ def compare_time_resnet18():
     axs[1].set_ylabel('Algorithm Name')
     # plt.tight_layout()
     plt.savefig(PLOTDIR + '/compare_time.png')
+
+def compare_memory():
+
+    colName = ['C P', 'W SVD', 'S SVD', 'AdaRound', 'CLE + BC' ]
+    
+    channel_resnet18 = np.array(list(zip(*[(5.9877166748046875, 2347.75), (5.322246551513672, 2347.75), (5.932491302490234, 2347.75), (5.547443389892578, 2417.75), (5.955879211425781, 2347.75), (5.724559783935547, 2347.75), (5.965583801269531, 2457.75), (5.558692932128906, 2347.75)])))
+    weightSVD_resnet18 = np.array(list(zip(*[(4.9183502197265625, 2660.75), (4.33245849609375, 2660.75), (4.370265960693359, 2660.75), (4.3753204345703125, 2660.75), (4.344554901123047, 2306.75), (4.312370300292969, 2306.75), (4.3216552734375, 2306.75), (4.323333740234375, 2306.75)])))
+    spatialSVD_resnet18 = np.array(list(zip(*[(4.410438537597656, 2081.75), (4.446521759033203, 2081.75), (4.429775238037109, 2081.75), (4.4181671142578125, 2081.75), (4.425960540771484, 2081.75), (4.388862609863281, 2081.75), (4.4202728271484375, 2081.75), (4.461154937744141, 2081.75)])))
+    adaround_resnet18 = np.array(list(zip(*[(4.282390594482422, 2017.75), (4.346527099609375, 2009.75), (4.343418121337891, 2007.75)])))
+    cle_bc_resnet18 = np.array(list(zip(*[(3.9810256958007812, 2038.75), (3.9981689453125, 2038.75), (4.0495758056640625, 2038.75)])))
+    
+    channel_resnet50 = np.array(list(zip(*[(14.816150665283203, 2941.75), (12.694087982177734, 2941.75), (12.42123794555664, 2941.75), (8.4573974609375, 2941.75), (13.638538360595703, 2941.75), (11.165534973144531, 2941.75), (11.165771484375, 2941.75), (11.442974090576172, 2941.75)])))
+    weightSVD_resnet50 = np.array(list(zip(*[(4.336017608642578, 2042.75), (4.314155578613281, 2042.75), (4.3668212890625, 2042.75), (4.343132019042969, 2042.75), (4.339973449707031, 2400.75), (4.356632232666016, 2400.75), (4.388874053955078, 2400.75), (4.241703033447266, 2400.75)])))
+    spatialSVD_resnet50 = np.array(list(zip(*[(4.453243255615234, 2021.75), (4.461597442626953, 2021.75), (4.466957092285156, 2021.75), (4.4745941162109375, 2021.75), (4.477317810058594, 2021.75), (4.479930877685547, 2021.75), (4.4957427978515625, 2021.75), (4.508365631103516, 2021.75)])))
+    adaround_resnet50 = np.array(list(zip(*[(4.403831481933594, 2317.75), (3.7180709838867188, 2321.75), (4.195941925048828, 2321.75)])))
+    cle_bc_resnet50 = np.array(list(zip(*[(4.180061340332031, 2292.75), (4.1720733642578125, 2292.75), (4.167217254638672, 2222.75)])))
+
+    fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(10, 12))
+
+    resnet18_memory_yerr =  [   channel_resnet18[0].max()-channel_resnet18[0].min(),\
+    weightSVD_resnet18[0].max()-weightSVD_resnet18[0].min(), spatialSVD_resnet18[0].max()-spatialSVD_resnet18[0].min(),\
+        adaround_resnet18[0].max() - adaround_resnet18[0].min(), cle_bc_resnet18[0].max() - cle_bc_resnet18[0].min()]
+    resnet18_memory_yerr = [0.5*x for x in resnet18_memory_yerr]
+    
+    axs[0, 0].bar(colName, [channel_resnet18[0].mean(), weightSVD_resnet18[0].mean(), spatialSVD_resnet18[0].mean(), adaround_resnet18[0].mean(), cle_bc_resnet18[0].mean()], color = '#8f80ca', yerr = resnet18_memory_yerr)
+    axs[0, 0].set_title('Algorithm Memory Cost on ResNet18')
+    axs[0, 0].set_xlabel('Algorithm Name')
+    axs[0, 0].set_ylabel('Memory Cost(GB)')
+
+    resnet50_memory_yerr =  [   channel_resnet50[0].max()-channel_resnet50[0].min(),\
+    weightSVD_resnet50[0].max()-weightSVD_resnet50[0].min(), spatialSVD_resnet50[0].max()-spatialSVD_resnet50[0].min(),\
+        adaround_resnet50[0].max() - adaround_resnet50[0].min(), cle_bc_resnet50[0].max() - cle_bc_resnet50[0].min()]
+    resnet50_memory_yerr = [0.5*x for x in resnet50_memory_yerr]
+    axs[0, 1].bar(colName, [channel_resnet50[0].mean(), weightSVD_resnet50[0].mean(), spatialSVD_resnet50[0].mean(), adaround_resnet50[0].mean(), cle_bc_resnet50[0].mean()], color = '#686eba', yerr = resnet50_memory_yerr)
+    axs[0, 1].set_title('Algorithm Memory Cost on ResNet50')
+    axs[0, 1].set_xlabel('Algorithm Name')
+    axs[0, 1].set_ylabel('Memory Cost(GB)')
+
+    resnet18_video_memory_yerr =  [   channel_resnet18[1].max()-channel_resnet18[1].min(),\
+    weightSVD_resnet18[1].max()-weightSVD_resnet18[1].min(), spatialSVD_resnet18[1].max()-spatialSVD_resnet18[1].min(),\
+        adaround_resnet18[1].max() - adaround_resnet18[1].min(), cle_bc_resnet18[1].max() - cle_bc_resnet18[1].min()]
+    resnet18_video_memory_yerr = [0.5*x for x in resnet18_video_memory_yerr]
+
+    axs[1, 0].bar(colName, [channel_resnet18[1].mean(), weightSVD_resnet18[1].mean(), spatialSVD_resnet18[1].mean(), adaround_resnet18[1].mean(), cle_bc_resnet18[1].mean()], color = '#abc88b', yerr = resnet18_video_memory_yerr)
+    axs[1, 0].set_title('Algorithm Video Memory Cost on ResNet18')
+    axs[1, 0].set_xlabel('Algorithm Name')
+    axs[1, 0].set_ylabel('Memory Cost(MB)')
+
+    resnet50_video_memory_yerr =  [   channel_resnet50[1].max()-channel_resnet50[1].min(),\
+    weightSVD_resnet50[1].max()-weightSVD_resnet50[1].min(), spatialSVD_resnet50[1].max()-spatialSVD_resnet50[1].min(),\
+        adaround_resnet50[1].max() - adaround_resnet50[1].min(), cle_bc_resnet50[1].max() - cle_bc_resnet50[1].min()]
+    resnet50_video_memory_yerr = [0.5*x for x in resnet50_memory_yerr]
+
+    axs[1, 1].bar(colName, [channel_resnet50[1].mean(), weightSVD_resnet50[1].mean(), spatialSVD_resnet50[1].mean(), adaround_resnet50[1].mean(), cle_bc_resnet50[1].mean()], color = '#88b365', yerr = resnet50_video_memory_yerr)
+    axs[1, 1].set_title('Algorithm Video Memory Cost on ResNet50')
+    axs[1, 1].set_xlabel('Algorithm Name')
+    axs[1, 1].set_ylabel('Memory Cost(MB)')
+
+    plt.tight_layout()
+    plt.savefig(PLOTDIR + '/compare_memory.png')
 
 
 
@@ -316,4 +384,5 @@ if __name__ == '__main__':
     # adaround()
     # cle_bc_resnet18()
     # cle_bc_resnet50()
-    compare_time_resnet18()
+    # compare_time()
+    compare_memory()
--- handleMonitorData.py
+++ handleMonitorData.py
@@ -15,12 +15,11 @@ def findMax(filen):
 if __name__ == '__main__':
     ans = []
     count = 0
-    with  open('./monitorLog/adaround_resnet50.log', mode='r') as f:
+    with  open('./monitorLog/cle_bc.txt', mode='r') as f:
         lines = f.readlines()
-        sections = [ 722, 913, 1103, 1296, 1489, 1681, 1875]
-        # for trueFile in [lines[ 530:sections[0]], lines[sections[0]:sections[1]], lines[sections[1]:sections[2]], lines[sections[2]:sections[3]]]:
-        #     ans.append(findMax(trueFile))
-        # for falseFile in [lines[sections[3]:sections[4]], lines[sections[4]:sections[5]], lines[sections[5]:sections[6]], lines[sections[6]:]]:
-        #     ans.append(findMax(falseFile))
-        ans.append(findMax(lines))
+        sections = [ 32, 64, 96, 186, 276, 366]
+        for trueFile in [lines[ 0:sections[0]], lines[sections[0]:sections[1]], lines[sections[1]:sections[2]]]:
+            ans.append(findMax(trueFile))
+        for falseFile in [lines[sections[2]:sections[3]], lines[sections[3]:sections[4]], lines[sections[4]:sections[5]]]:
+            ans.append(findMax(falseFile))
     print(ans)
